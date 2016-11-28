406b7c1d25f37a3e55a7e517943103e754fb5bb9
yef <yef@DESKTOP-DD9VBI7>
2016-11-27 22:24:01 -0500

    Cambio en interfaz

--- src/java/UserWizard.java
+++ src/java/UserWizard.java
@@ -5,30 +5,65 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.event.FlowEvent;
- 
+
 @ManagedBean
 @ViewScoped
 public class UserWizard implements Serializable {
- 
-   
+
     private boolean skip;
-         
-     
+    private boolean cuentaprovedor;
+    private int tipocontrato;
+    private String[] selectedClausulas;
+    private String[] selectedAcuerdos;
+
     public boolean isSkip() {
         return skip;
     }
- 
+
     public void setSkip(boolean skip) {
         this.skip = skip;
     }
-     
+
     public String onFlowProcess(FlowEvent event) {
-        if(skip) {
+        if (skip) {
             skip = false;   //reset in case user goes back
             return "confirm";
-        }
-        else {
+        } else {
             return event.getNewStep();
         }
     }
+
+    public boolean isCuentaprovedor() {
+        return cuentaprovedor;
+    }
+
+    public void setCuentaprovedor(boolean cuentaprovedor) {
+        this.cuentaprovedor = cuentaprovedor;
+    }
+
+    public int getTipocontrato() {
+        return tipocontrato;
+    }
+
+    public void setTipocontrato(int tipocontrato) {
+        this.tipocontrato = tipocontrato;
+    }
+
+    public String[] getSelectedClausulas() {
+        return selectedClausulas;
+    }
+
+    public void setSelectedClausulas(String[] selectedClausulas) {
+        this.selectedClausulas = selectedClausulas;
+    }
+
+    public String[] getSelectedAcuerdos() {
+        return selectedAcuerdos;
+    }
+
+    public void setSelectedAcuerdos(String[] selectedAcuerdos) {
+        this.selectedAcuerdos = selectedAcuerdos;
+    }
+    
+
 }
